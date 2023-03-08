915bc4aff015a9667288aaf885e3d4c5e53558de
ShinyDuck21 <84870282+ShinyDuck21@users.noreply.github.com>
2023-03-08 16:32:24 -0600

    changes

---
+++ src/commands/tools/rock.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { rock } = require("../../../data.json")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("rockroulette")
+        .setDescription("THE ROCK")
+        .addSubcommand(public =>
+            public.setName("public")
+                .setDescription("Sends to everyone"))
+        .addSubcommand(incognito =>
+            incognito.setName("incognito")
+                .setDescription("Sends to you and just you")),
+
+    async execute(interaction) {
+        switch (interaction.options.getSubcommand()) {
+            case "public":
+                const publicimage = rock[Math.floor(Math.random() * rock.length)];
+
+                await interaction.reply({ content: publicimage })
+                break;
+            case "incognito":
+                const privateimage = rock[Math.floor(Math.random() * rock.length)];
+
+                await interaction.reply({ content: privateimage, ephemeral: true })
+        }
+    }
+
+}
