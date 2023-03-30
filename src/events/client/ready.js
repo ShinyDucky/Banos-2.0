2808643cb4161e6e261bbcf64f8650249ced78a3
ShinyDuck <84870282+ShinyDuck21@users.noreply.github.com>
2023-03-30 03:27:56 +0000

    Status update

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -8,10 +8,7 @@ module.exports = {
 			process: process.pid,
 			status: 'idle'
 		});
-		client.user.setActivity('p.OS try to overtake me in superiority.', { type: ActivityType.Watching/*, url: 'https://www.twitch.tv/shinyduck21'*/ });
+		client.user.setActivity('you', { type: ActivityType.Watching/*, url: 'https://www.twitch.tv/shinyduck21'*/ });
 		console.log(`Ready!!! ${client.user.tag} is logged in and online.`);
-
-		let clientguilds = client.guilds.cache;
-		console.log(clientguilds.map(g => g.id) || "None")
 	}, // 909944891893755954
 };
